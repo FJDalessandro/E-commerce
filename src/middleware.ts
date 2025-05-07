8da44a2d64a62d586fa2e520d4d7c03866fb39fd
Francisco <franciscojdalessandro@gmail.com>
2025-05-07 11:20:06 +0200

    Solucion al deploy

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { NextResponse, NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
     const { pathname, origin } = request.nextUrl;
 
-    if ((pathname === "/dashboard" || pathname === "/cart" || pathname === "/examen") && !request.cookies.get("userSession")?.value) {
+    if ((pathname === "/dashboard" || pathname === "/cart") && !request.cookies.get("userSession")?.value) {
         const loginUrl = new NextURL("/login", origin);
         return NextResponse.redirect(loginUrl);
     } else {
